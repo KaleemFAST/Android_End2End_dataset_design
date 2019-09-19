6462df486c9c0f753d70ecc72820342122fd208e
Kaleem Nawaz Khan <18640571+KaleemFAST@users.noreply.github.com>
2019-09-19 12:10:01 +0500

    Update Dex_extraction.sh

--- Dex_extraction.sh
+++ Dex_extraction.sh
@@ -2,5 +2,5 @@
 cd /directory1/directory2/directory3/All_APKs  #path to the apks folder
 for i in DataSet/*
 do
-  apktool d -r -s $i     
+  apktool d -r -s $i   # Apktool is a reverse engineering tool for Android apks  
 done
